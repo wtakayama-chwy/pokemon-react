6a7524ee9236550c90468d0d1fb27170bb48d064
William S. Takayama <william-sobreira@Hotmail.com>
2020-05-19 17:45:47 -0300

    Setting up initial project

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export default function Home() {
+    // Variables with state
+
+    return(
+        <Fragment>
+            <h1>Oi mundo</h1>
+        </Fragment>
+    )
+}
